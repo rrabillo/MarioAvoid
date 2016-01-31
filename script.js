5e9999a2cab3d750d527cf982cfdff40fe5eb5b3
unknown <richard.rabillon@live.fr>
2016-02-01 00:24:31 +0100

    Adde very basic collisions et pause/resume

--- script.js
+++ script.js
@@ -4,21 +4,23 @@ $(document).ready(function(){
 // Construction de la logique 
 
 
-var underGravity = []; // Création d'un array qui contiendra les objets soumis à la gravité
-zoneDeJeu = function(selector){// Définition d'un objet "zone de jeu", qui nous permettra notamment de détecter certaines colisions (gauche et droite du viewport) et eclenche la gravité
+var underPhysics = []; // Création d'un array qui contiendra les objets soumis à la gravité
+var test = document.getElementById("sol")
+zoneDeJeu = function(selector , ground){// Définition d'un objet "zone de jeu", qui nous permettra notamment de détecter certaines colisions (gauche et droite du viewport) et eclenche la gravité
 	this.htmlElement = selector;
+	this.sol = ground;
 	this.leftBorder = selector.offsetLeft;	
 	this.height = selector.offsetHeight;
 	zoneDeJeu.prototype.gravity = function(){				
-		for(i = 0; i < underGravity.length; i++){
-			var object = underGravity[i];
+		for(i = 0; i < underPhysics.length; i++){
+			var object = underPhysics[i];
 			if(!object.jump()){
-				if(object.htmlElement.offsetTop < this.height - object.htmlElement.offsetHeight){
+				if(object.htmlElement.offsetTop < this.sol.offsetTop - object.htmlElement.offsetHeight){
 					object.pos.y += 10;
 					object.htmlElement.style.top = object.pos.y+"px";
 					object.falling = true;
-				}else if(object.htmlElement.offsetTop >= this.height - object.htmlElement.offsetHeight){
-					object.pos.y = this.height - object.htmlElement.offsetHeight;
+				}else if(object.htmlElement.offsetTop >= this.sol.offsetTop - object.htmlElement.offsetHeight){
+					object.pos.y = this.sol.offsetTop  - object.htmlElement.offsetHeight;
 					object.htmlElement.style.top = object.pos.y+"px";
 					object.falling = false;
 					
@@ -26,12 +28,48 @@ zoneDeJeu = function(selector){// Définition d'un objet "zone de jeu", qui nous
 			}
 		}
 	}
+	zoneDeJeu.prototype.collisions = function(){
+		var elwidth = test.offsetWidth,
+		elheight = test.offsetHeight,
+		elTop = test.offsetTop,
+		elLeft = test.offsetLeft,
+		elRight = elLeft + elwidth,
+		elBottom = elTop + elheight;
+		for(i = 0; i < underPhysics.length; i++){
+			var object = underPhysics[i];
+			if(elLeft < object.pos.x + object.width && elRight > object.pos.x && elTop < object.pos.y + object.height && elBottom > object.pos.y ){
+				
+			}
+			
+			
+		}
+
+	}
+}
+	while(document.getElementsByClassName('bullet-bill').length < 3){
+  			$('body').append('<div class="bullet-bill"></div>');
+	}	
+obstacle = function(selector){
+	this.status = true; // Si l'objet a disparu ou non
+	this.htmlElement = selector;
+	for(i = 0; i < selector.length; i++){
+		this.height = selector[i].offsetHeight;
+		this.width = selector[i].offsetWidth;
+	}
+	this.pos = {x : area.htmlElement.offsetWidth - this.width , y : 0}; 
+
+	obstacle.prototype.move = function(){
+		this.pos.x -= 5;
+		this.htmlElement[0].style.left = this.pos.x+"px";
+	}
+
 } 
 joueur = function(selector , speed ){ // Définition d'un objet joueur
 	this.htmlElement = selector;
 	this.width = selector.offsetWidth;
 	this.height = selector.offsetHeight;
 	this.velocity = speed;
+	this.collision = false;
 	this.pos = {x : 0 , y : 0};
 	this.falling = true; // Pour empécher le saut alors que le personnage est en l'air
 	this.jumpPower = 5;
@@ -39,14 +77,15 @@ joueur = function(selector , speed ){ // Définition d'un objet joueur
 	this.reverse = false;
 	joueur.prototype.move = function() { // Méthode pour bouger à gauche et à droite (On pourrait l'optimiser car c'est très brouillon mais manque de temps)
 		if (key.right === true) { // Si on appui sur la fleche de droite, on incremente la propriété css "left" par la vitesse passée à l'objet
-        	this.pos.x += this.velocity; 
+				this.pos.x += this.velocity; 	
+
         	if(key.up === false){
         		this.htmlElement.className = ('walking-right');
         	}       	
     		 this.reverse = false;
     	} 
     	else if (key.left === true){ // Inverse du dessus
-    		this.pos.x -= this.velocity;
+    			this.pos.x -= this.velocity;
     		if(key.up === false){
         		this.htmlElement.className = ('walking-left');
         	}    
@@ -105,11 +144,17 @@ joueur = function(selector , speed ){ // Définition d'un objet joueur
 			this.pressJump = 0;
 		}
 	}
-	underGravity.push(this);
+	underPhysics.push(this);
 
 }
+
 mario = new joueur(document.getElementById("personnage") ,7); // Création du joueur
-area = new zoneDeJeu(document.body); // Notre zone est le body.
+area = new zoneDeJeu(document.body , document.getElementById("sol")); // Notre zone est le body.
+bulletbill = new obstacle(document.getElementsByClassName("bullet-bill"));
+console.log(bulletbill);
+function getRandomInt(min, max) {
+  	/*console.log(Math.floor(Math.random() * (max - min)) + min);*/
+}
 // Contrôles
 var key  = { // On créé un objet clef qui permettra d'enregistrer de détecter si une touche est enfoncée ou non (et la détection de plusieurs touches enfoncées)
 	left:false,
@@ -117,6 +162,7 @@ var key  = { // On créé un objet clef qui permettra d'enregistrer de détecter
 	up:false,
 }
 function keyDown(e) {
+	
     if (e.keyCode === 39) {
         key.right = true;
     } else if (e.keyCode === 37) {
@@ -136,6 +182,9 @@ function keyUp(e) {
     if (e.keyCode === 38) {
         key.up = false;
     }
+    if (e.keyCode === 32){
+       	
+    }
 }
 document.addEventListener('keydown', keyDown, false);
 document.addEventListener('keyup', keyUp, false);
@@ -143,8 +192,22 @@ document.addEventListener('keyup', keyUp, false);
 	mario.jump();
 // Il est nécéssaire de créer une loop pour que les fonctions soient lancées constamment
 function loop(){
+	area.collisions();
 	area.gravity();
 	mario.move();
+	bulletbill.move();
 }
-setInterval(loop,15);
+ gameloop = setInterval(loop,15);
+
+
+$('body').click(function(){ // Fonction de pause
+	if($(this).hasClass('paused')){
+		$(this).removeClass('paused');
+		gameloop=setInterval(loop,15);;
+	}
+	else{
+		$(this).addClass('paused');
+		clearInterval(gameloop);
+	}
+});
 });
